4149b50328a93c0a18d790090d4d3e3fd26cce73
Rodrigo Lungui <rodrigo.lungui@warren.com.br>
2022-05-05 09:43:09 -0300

    feat: update configs and adjustments

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+// @ts-ignore
 import * as lists from './lists/*.json'
+
+// @ts-ignore
 import * as enums from './enums/*.json'
+
+// @ts-ignore
 import * as models from './models/*.json'
 
 export { lists, enums, models }
